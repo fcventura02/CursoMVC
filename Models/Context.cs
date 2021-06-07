7b0d39e8113780f02e3be487eb807892525c4344
fcventura02 <filipecvr4@gmail.com>
2021-06-07 20:24:23 -0300

    Frist commit

---
+++ Models/Context.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias {get; set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer(@"Server=localhost;User ID=Sa;Password=8450.filipe;Database=Cursomvc;");
+        }
+    }
+}
